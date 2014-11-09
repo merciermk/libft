5422aa254993ff44318536ae58852c51eb686775
Mike MERCIER <mmercier@e2r12p6.42.fr>
2014-11-09 17:45:30 +0100

    libft a jour

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmercier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 12:11:48 by mmercier          #+#    #+#             */
-/*   Updated: 2014/11/08 21:40:09 by mmercier         ###   ########.fr       */
+/*   Updated: 2014/11/09 16:38:16 by mmercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void        ft_bzero(void *s, size_t n);
 void *		ft_memcpy(void *dst, const void *src, size_t n);
 void *		ft_memccpy(void *dst, const void *src, int c, size_t n);
 void *		ft_memmove(void *dst, const void *src, size_t len);
-//void *		ft_memchr(const void *s, int c, size_t n);
+void *		ft_memchr(const void *s, int c, size_t n);
 //int			ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t		ft_strlen(const char *s);
 char *		ft_strdup(const char *s1);
