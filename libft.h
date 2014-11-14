4bab6144e3930d5f5faf71189eaaee58d276f97e
Mike MERCIER <mmercier@e2r12p6.42.fr>
2014-11-14 14:23:58 +0100

    del

--- libft.h
+++ libft.h
@@ -58,7 +58,6 @@ int			ft_strequ(char const *s1, char const *s2);
 int			ft_strnequ(char const *s1, char const *s2, size_t n);
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_strtrim(char const *s);
 char		**ft_strsplit(char const *s, char c);
 int			ft_atoi(const char *str);
 void		ft_putchar(char c);
